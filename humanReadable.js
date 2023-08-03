1e09ab1824de5d396ab7b2130d9b2c4c555429c7
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 16:18:29 +0100

    Refactoring of code.

--- humanReadable.js
+++ humanReadable.js
@@ -1,22 +1,18 @@
 function humanReadable(seconds) {
-  if (seconds < 60) {
-    return `00:00:${seconds}`;
-  } else {
-    let hours = Math.floor(seconds / 3600).toLocaleString("en-US", {
-      minimumIntegerDigits: 2,
-      useGrouping: false,
-    });
-    let secondsRemaining = seconds % 3600; // ready to work out minutes
-    let minutes = Math.floor(secondsRemaining / 60).toLocaleString("en-US", {
-      minimumIntegerDigits: 2,
-      useGrouping: false,
-    }); // minutes worked out
-    secondsRemaining = (secondsRemaining % 60).toLocaleString("en-US", {
-      minimumIntegerDigits: 2,
-      useGrouping: false,
-    });
-    return `${hours}:${minutes}:${secondsRemaining}`;
-  }
+  let hours = Math.floor(seconds / 3600).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  let secondsRemaining = seconds % 3600; // ready to work out minutes
+  let minutes = Math.floor(secondsRemaining / 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  }); // minutes worked out
+  secondsRemaining = (secondsRemaining % 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  return `${hours}:${minutes}:${secondsRemaining}`;
 }
 
 module.exports = humanReadable;
